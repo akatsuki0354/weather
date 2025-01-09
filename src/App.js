64986ad822ba1ba78d865dfd318b29eed2ccbab7
akatsuki0354 <francogregorio2004@gmail.com>
2025-01-10 00:34:34 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,77 +1,172 @@
 import React, { useState, useEffect } from 'react';
 import CloudIcon from '@mui/icons-material/Cloud';
+import AirIcon from '@mui/icons-material/Air';
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import NightsStayIcon from '@mui/icons-material/NightsStay';
+import PublicIcon from '@mui/icons-material/Public';
 import '../src/App.css'
 
 function App() {
-  const [temp, setTemp] = useState(null);
-  const [Country, setCountry] = useState('');
-  const [InvalidCity, setInvalidCity] = useState("");
+  const [max_temp, setMax_Temp] = useState(null);
+  const [temp, setTemp] = useState('');
+  const [min_temp, setMin_Temp] = useState('');
+  const [sunrise, setSunRise] = useState('');
+  const [sunset, setSunSet] = useState('');
+  const [conditions, setCondition] = useState('');
+  const [name, setname] = useState('');
+  const [Invalid_City, setInvalid_City] = useState("");
+  const [region, setRegion] = useState("");
+  const [country, setCountry] = useState("");
+  const [lon, setLon] = useState("");
+  const [lat, setLat] = useState("");
+  const [local_Time, setLocal_Time] = useState("");
+  const [moon_Phase, setMoon_Phase] = useState("");
+  const [maxwind_kph, setMaxwind_kph] = useState("");
+  const [wind_kph, setWind_kph] = useState("");
+  const [moonrise, setMoonrise] = useState("");
+  const [moonset, setMoonset] = useState("");
+
+
 
 
 
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (Country.trim() === '') {
-      setInvalidCity("Please enter a valid Country or City")
+    if (name.trim() === '') {
+      setInvalid_City("Please enter a valid name or City")
       return;
     } else {
-      setInvalidCity("");
+      setInvalid_City("");
     }
-    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${Country}&aqi=no`;
+    //  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    // const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${name}&aqi=no`;
 
+
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=6ba551e0ee3a40229dd80510250801&q=${name}&days=1&aqi=no&alerts=no`;
     try {
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
       }
       const data = await response.json();
-      setTemp(data.current.temp_c);
+      setLon(data.location.lon);
+      setLat(data.location.lat);
+      setMoonrise(data.forecast.forecastday[0].astro.moonrise);
+      setMoonset(data.forecast.forecastday[0].astro.moonset);
+      setMoon_Phase(data.forecast.forecastday[0].astro.moon_phase);
+      setLocal_Time(data.location.localtime);
+      setWind_kph(data.current.wind_kph);
+      setMaxwind_kph(data.forecast.forecastday[0].day.maxwind_kph);
+      setTemp(data.current.temp_c)
+      setSunRise(data.forecast.forecastday[0].astro.sunrise)
+      setSunSet(data.forecast.forecastday[0].astro.sunset)
+      setCondition(data.current.condition.text)
+      setMax_Temp(data.forecast.forecastday[0].day.maxtemp_c);
+      setMin_Temp(data.forecast.forecastday[0].day.mintemp_c);
+      setRegion(data.location.region)
+      setCountry(data.location.country)
     } catch (error) {
       console.error('Failed to fetch weather data:', error);
       alert('Failed to fetch weather data. Please try again.');
     }
   };
+
+  useEffect(() => {
+    fetch("https://api.weatherapi.com/v1/forecast.json?key=6ba551e0ee3a40229dd80510250801&q=${name}&days=1&aqi=no&alerts=no")
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }, []);
   return (
-    <div className="App">
+    <div className="App ">
       <div>
-        <div className='text-muted'>
-          <center className='mt-5'>
-            <h1>Weather App</h1>
+        <div className='text-muted mt-4'>
+          <center>
+            <h1 className='text-5xl mb-2 font-semibold'>Weather App</h1>
             <p >
               <CloudIcon /> API by WeatherApi
             </p>
-            <div>
 
+          </center>
+        </div>
+        <div className='lg:flex lg:justify-around container'>
+          <form onSubmit={handleSubmit}>
+            <center className='text text-danger'>
+              <input
+                class="form-control mt-3 lg:w-64 "
+                type="text"
+                placeholder='region or city'
+                value={name}
+                onChange={(e) => setname(e.target.value)} />
+
+
+              <p>{Invalid_City}</p>
+            </center>
+            <div className='button'>
+              <button type="submit" className='btn w-64 btn-success mt-2 ' >Get Weather</button>
             </div>
-          </center>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <input
-            class="form-control mt-5 "
-            type="text"
-            placeholder='Country or city'
-            value={Country}
-            onChange={(e) => setCountry(e.target.value)} />
-          <center className='text text-danger'>
-            <p>{InvalidCity}</p>
-          </center>
-          <div className='d-grid'>
-            <button type="submit" className='btn btn-success w-full mt-3' >Get Weather</button>
-          </div>
-        </form>
-        <div className="mt-3 text-muted">
-          {temp === null ? (
-            <div><p>Enter a valid Country or city to get the weather</p></div>
-          ) : (
-            <div><h2>Temperature: {temp} &#8451;</h2></div>
-          )}
-        </div>
-      </div>
 
-      <div className='crdts text-muted'>
-        © Franco Gregorio 2023. All Rights reserved.
+            <div>
+              {max_temp === null ? (
+                <div> <center><p className='text-2xl font-semibold'></p></center></div>
+              ) : (
+                <div className='text text-muted text-lg'>
+                  <center className='mt-3'>
+                    <p className='text-3xl mb-3'><NightsStayIcon/> {moon_Phase}</p>
+                    <p>{local_Time}</p>
+                  </center>
+                  <div className='flex space-x-10 mt-3'>
+                    <div>
+                      <p><PublicIcon/> Lat : {lat}</p>
+                      <p><NightlightIcon/> Moonrise : {moonrise}</p>
+                      <p><AirIcon/> wind_kph : {wind_kph}</p>
+                    </div>
+                    <div>
+                      <p><PublicIcon/> Lon : {lon}</p>
+                      <p><NightlightIcon/> Moonset : {moonset}</p>
+                      <p><AirIcon/> maxwind_kph : {maxwind_kph}</p>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </form>
+          <div className="mt-3 text-muted">
+            {max_temp === null ? (
+              <div> <center><p className='text-2xl font-semibold'>WEATHER UPDATE</p></center></div>
+            ) : (
+
+              <div>
+                <center>
+                  <h4 className='md:text-xl lg:text-3xl mb-2'>{country}, {region}</h4>
+                  <h1 className='md:text-2xl lg:text-4xl mb-2'> {temp} &#8451;</h1>
+                  <h5 className='md:text-xl lg:text-2xl text-center mb-2'><CloudIcon /> {conditions}</h5>
+                </center>
+                <div className='condtion_and_tem mt-10 gap-5 md:gap-10 lg:space-x-10'>
+                  <div>
+                    <center>
+                      <p className='text-xl'>Highest : {max_temp} &#8451;</p>
+                      <p className='lg:text-2xl mt-3 font-semibold'>Sunrise</p>
+                      <img src="https://img.icons8.com/external-tulpahn-outline-color-tulpahn/64/000000/external-sunrise-weather-tulpahn-outline-color-tulpahn.png" className='mt-3 mb-3' alt="sunrise" />
+                      <p className='lg:text-2xl font-semibold'>{sunrise}</p>
+                    </center>
+                  </div>
+                  <div>
+                    <center>
+                      <p className='text-xl'>Lowest : {min_temp} &#8451;</p>
+                      <p className='lg:text-2xl mt-3 font-semibold'>Sunset </p>
+                      <img className='mt-3 mb-3' src="https://img.icons8.com/external-tulpahn-outline-color-tulpahn/64/000000/external-sunrise-weather-tulpahn-outline-color-tulpahn-1.png" alt="" />
+                      <p className='lg:text-2xl font-semibold'>{sunset}</p>
+                    </center>
+                  </div>
+                </div>
+                <div>
+
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/youtube.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function youtube() {
+  return (
+    <div>
+        https://www.googleapis.com/youtube/v3/search?part=snippet&q=programming&type=video&key=AIzaSyB0aiq6IlS3_JAT8P8KEDdolWGf74519Jc
+    </div>
+  )
+}
+
+export default youtube
